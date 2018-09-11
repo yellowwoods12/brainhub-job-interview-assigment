7eb3defded5af419a8e9bbc3c05bb7439a920ec9
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-09-12 01:14:36 +0200

    Endpoint POST /user => checking if User already exists by its email

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { insertUser } = require('../services/userServices');
+const { insertUser, getUsers } = require('../services/userServices');
 
 router.route('/user')
     .post((req, res) => { insertUser(req, res) });
+
+router.route('/users')
+    .get((req, res) => { getUsers(req, res) });
 
 module.exports = router;
--- server/services/userServices.js
+++ server/services/userServices.js
@@ -1,37 +1,55 @@
 const User = require('../models/User');
-const { validationResult } = require('express-validator/check');
+const { body, validationResult } = require('express-validator/check');
 
 function insertUser(req, res) {
-    console.log(req.body);
+
     req.check('firstName', 'First name value is required!').notEmpty();
     req.check('lastName', 'Last name value is required!').notEmpty();
     req.check('email', 'Email value is required!').notEmpty();
     req.check('email', 'Invalid Email!').isEmail();
     req.check('eventDate', 'Event Date is required!').notEmpty();
-
+    
     let errors = req.validationErrors();
 
     if (errors) {
         return res.status(422).json({ errors: errors });
     } else {
-        let user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            eventDate: req.body.eventDate
-        });
-        user.save((err) => {
-            if(err) {
-                res.send(err);
+        User.findOne({ email: req.body.email }, (err, user) => {
+            if(user !== null) {
+                res.json({
+                    message: 'User email already exists!'
+                })
+            } else {
+                let user = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    eventDate: req.body.eventDate
+                });
+                user.save((err) => {
+                    if(err) {
+                        res.send(err);
+                    }
+                    res.json({
+                        message: 'User successfully added!',
+                        body: user
+                    });
+                });
             }
-            res.json({
-                message: 'User successfully added!',
-                body: user
-            });
-        });
+        });    
     }
 }
 
+function getUsers(req, res) {
+    User.find((err, expenses) => {
+        if(err) {
+            res.send(err);
+        }
+        res.json(expenses);
+    });
+}
+
 module.exports = {
-    insertUser
+    insertUser,
+    getUsers
 };
